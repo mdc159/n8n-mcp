b6c30c8df91904f83cd2d55509ece4897d4564d1
github-action-benchmark <github@users.noreply.github.com>
2025-09-29 20:06:15 +0000

    add n8n-mcp Benchmarks (customSmallerIsBetter) benchmark result for 840549726303fe8a58f2468f6d068413b7eb7e55

--- benchmarks/data.js
+++ benchmarks/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1758907988817,
+  "lastUpdate": 1759176375696,
   "repoUrl": "https://github.com/czlonkowski/n8n-mcp",
   "entries": {
     "n8n-mcp Benchmarks": [
@@ -52,6 +52,58 @@ window.BENCHMARK_DATA = {
             "range": "0.3349",
             "unit": "ms",
             "extra": "14636 ops/sec"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "56956555+czlonkowski@users.noreply.github.com",
+            "name": "Romuald Członkowski",
+            "username": "czlonkowski"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "840549726303fe8a58f2468f6d068413b7eb7e55",
+          "message": "Merge pull request #238 from czlonkowski/fix/validation-false-positives\n\nfix: resolve validation false positives for Google Drive and Code nodes (v2.14.2)",
+          "timestamp": "2025-09-29T22:04:51+02:00",
+          "tree_id": "540884612b56bfcb06edec318e7a911101285f51",
+          "url": "https://github.com/czlonkowski/n8n-mcp/commit/840549726303fe8a58f2468f6d068413b7eb7e55"
+        },
+        "date": 1759176375425,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "sample - array sorting - small",
+            "value": 0.019,
+            "range": "0.2057",
+            "unit": "ms",
+            "extra": "52512 ops/sec"
+          },
+          {
+            "name": "sample - array sorting - large",
+            "value": 3.1769,
+            "range": "0.9382000000000001",
+            "unit": "ms",
+            "extra": "315 ops/sec"
+          },
+          {
+            "name": "sample - string concatenation",
+            "value": 0.0046,
+            "range": "0.2655",
+            "unit": "ms",
+            "extra": "215939 ops/sec"
+          },
+          {
+            "name": "sample - object creation",
+            "value": 0.0667,
+            "range": "0.30929999999999996",
+            "unit": "ms",
+            "extra": "14998 ops/sec"
           }
         ]
       }
