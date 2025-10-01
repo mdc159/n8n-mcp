e536cb8fe9ea52dd1714602928ba5af2218af426
github-action-benchmark <github@users.noreply.github.com>
2025-10-01 06:29:13 +0000

    add n8n-mcp Benchmarks (customSmallerIsBetter) benchmark result for f4dff6b8e12d1fe24d5cd5cb733b6842847252e5

--- benchmarks/data.js
+++ benchmarks/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1759019577974,
+  "lastUpdate": 1759300153083,
   "repoUrl": "https://github.com/mdc159/n8n-mcp",
   "entries": {
     "n8n-mcp Benchmarks": [
@@ -52,6 +52,58 @@ window.BENCHMARK_DATA = {
             "range": "0.3579",
             "unit": "ms",
             "extra": "15031 ops/sec"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "56956555+czlonkowski@users.noreply.github.com",
+            "name": "Romuald Członkowski",
+            "username": "czlonkowski"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "f4dff6b8e12d1fe24d5cd5cb733b6842847252e5",
+          "message": "Merge pull request #243 from czlonkowski/feature/execution-data-filtering\n\nfeat: Intelligent Execution Data Filtering for n8n_get_execution Tool",
+          "timestamp": "2025-10-01T00:21:57+02:00",
+          "tree_id": "cabfa56be93a0a16e54e26a8fe67f8750eecde96",
+          "url": "https://github.com/mdc159/n8n-mcp/commit/f4dff6b8e12d1fe24d5cd5cb733b6842847252e5"
+        },
+        "date": 1759300152295,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "sample - array sorting - small",
+            "value": 0.0193,
+            "range": "0.41900000000000004",
+            "unit": "ms",
+            "extra": "51920 ops/sec"
+          },
+          {
+            "name": "sample - array sorting - large",
+            "value": 3.2258,
+            "range": "1.0893000000000006",
+            "unit": "ms",
+            "extra": "310 ops/sec"
+          },
+          {
+            "name": "sample - string concatenation",
+            "value": 0.0047,
+            "range": "0.2844",
+            "unit": "ms",
+            "extra": "212015 ops/sec"
+          },
+          {
+            "name": "sample - object creation",
+            "value": 0.0674,
+            "range": "0.40349999999999997",
+            "unit": "ms",
+            "extra": "14829 ops/sec"
           }
         ]
       }
