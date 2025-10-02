0322e229f25d9b94749deb3d520d1308f3204274
github-action-benchmark <github@users.noreply.github.com>
2025-10-02 14:56:10 +0000

    add n8n-mcp Benchmarks (customSmallerIsBetter) benchmark result for 2f234780dd6f9d36e2cceb131491737342e75e49

--- benchmarks/data.js
+++ benchmarks/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1759313435009,
+  "lastUpdate": 1759416970199,
   "repoUrl": "https://github.com/czlonkowski/n8n-mcp",
   "entries": {
     "n8n-mcp Benchmarks": [
@@ -364,6 +364,58 @@ window.BENCHMARK_DATA = {
             "range": "0.3",
             "unit": "ms",
             "extra": "14951 ops/sec"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "56956555+czlonkowski@users.noreply.github.com",
+            "name": "Romuald Członkowski",
+            "username": "czlonkowski"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "2f234780dd6f9d36e2cceb131491737342e75e49",
+          "message": "Merge pull request #247 from czlonkowski/feature/p0-priorities-fixes\n\nfeat(p0-r1): Universal node type normalization to eliminate 80% of validation errors",
+          "timestamp": "2025-10-02T16:54:13+02:00",
+          "tree_id": "0d223a413d648276e7b3285341923363b4b5bb92",
+          "url": "https://github.com/czlonkowski/n8n-mcp/commit/2f234780dd6f9d36e2cceb131491737342e75e49"
+        },
+        "date": 1759416969474,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "sample - array sorting - small",
+            "value": 0.0195,
+            "range": "0.2993",
+            "unit": "ms",
+            "extra": "51235 ops/sec"
+          },
+          {
+            "name": "sample - array sorting - large",
+            "value": 3.438,
+            "range": "0.9804000000000004",
+            "unit": "ms",
+            "extra": "291 ops/sec"
+          },
+          {
+            "name": "sample - string concatenation",
+            "value": 0.0048,
+            "range": "0.2685",
+            "unit": "ms",
+            "extra": "210356 ops/sec"
+          },
+          {
+            "name": "sample - object creation",
+            "value": 0.0667,
+            "range": "0.36769999999999997",
+            "unit": "ms",
+            "extra": "15004 ops/sec"
           }
         ]
       }
